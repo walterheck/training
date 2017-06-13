b9fe12a18d9d2dd515422cc6aef8f435459b7f99
HashiCorp Training <training@hashicorp.com>
2017-06-13 15:31:08 +0000

    delete dnsimple record

--- dns.tf
+++ dns.tf
@@ -3,9 +3,3 @@ provider "dnsimple" {
   account = "hahafuckyou"
 }
 
-resource "dnsimple_record" "foobar" {
-  domain = "dnsimple_domain"
-  name   = "terraform"
-  value  = "${aws_instance.web.0.public_ip}"
-  type   = "A"
-}
